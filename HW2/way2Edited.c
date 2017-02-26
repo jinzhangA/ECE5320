7de04d4a4b54e8d3189df58f4112e7550763e3e8
Zhuo Chen <zc292@cornell.edu>
2017-02-25 22:12:21 -0500

    edit way2

--- HW2/way2Edited.c
+++ HW2/way2Edited.c
@@ -4,12 +4,13 @@
 #include <omp.h>
 #include <time.h>	/* for clock_gettime */
 
-#define N 4
+//#define N 4
 #define SEED 2
 #define BILLION 1000000000L
 
 int **mat;
 int **tempMat;
+uint64_t timer;
 
 void **generateMat();
 void printMat(int **mat);
@@ -17,7 +18,7 @@ void way2Sort();
 void copyMat();
 void creatTempMat();
 void mergeSort(int *value, int *index, int n);
-void mergeSortPara(int *values, int *indexs, int n)
+void mergeSortPara(int *values, int *indexs, int n);
 void merge(int *values, int *valueL, int *valueR, int *indexs, int *indexL, int *indexR, int leftCount, int rightCount);
 
 void **generateMat() {
@@ -41,7 +42,7 @@ void way2Sort(){
 	
 	uint64_t diff;
 	struct timespec start, end;
-	clock_gettime(CLOCK_MONOTONIC, &start);	
+	
 	 
 	for (i = 0; i < N - 1; i++){
 		size = N - i;
@@ -51,8 +52,11 @@ void way2Sort(){
 			indexs[j] = j + i;
 			values[j] = mat[j + i][i];
 		}
+		clock_gettime(CLOCK_MONOTONIC, &start);	
 		mergeSortPara(values, indexs, N - i);
-		
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+		timer += diff;
 		for (j = 0; j < N; j++){
 			for (k = 0; k < N; k++){
 				tempMat[j][k] = mat[j][k];
@@ -70,10 +74,9 @@ void way2Sort(){
 		free(values);
 		free(indexs);
 	}
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-	printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) diff);
-	printMat(mat);
+	
+	
+//	printMat(mat);
 }
 
 void mergeSort(int *values, int *indexs, int n){
@@ -106,40 +109,40 @@ void mergeSort(int *values, int *indexs, int n){
 	
 }
 
-//void mergeSortPara(int *values, int *indexs, int n){
-//	int mid, i, *valueL, *valueR, *indexL, *indexR;
-//	if (n < 2) return;
-//	mid = n/2;
-//	
-//	valueL = (int *)malloc(mid*sizeof(int));
-//	valueR = (int *)malloc((n - mid)*sizeof(int));
-//	
-//	indexL = (int *)malloc(mid*sizeof(int));
-//	indexR = (int *)malloc((n - mid)*sizeof(int));
-//	for (i = 0; i < mid; i++){
-//		valueL[i] = values[i];
-//		indexL[i] = indexs[i];
-//	}
-//	
-//	for (i = mid; i < n; i++){
-//		valueR[i - mid] = values[i];
-//		indexR[i - mid] = indexs[i];
-//	}
-//	#pragma omp parallel sections
-//	{
-//		#pragma omp section
-//		mergeSortPara(valueL, indexL, mid);
-//		#pragma omp section
-//		mergeSortPara(valueR, indexR, n - mid);
-//		
-//	}
-//	merge(values, valueL, valueR, indexs, indexL, indexR, mid, n - mid);
-//	free(valueL);
-//	free(valueR);
-//	free(indexL);
-//	free(indexR);
-//	
-//}
+void mergeSortPara(int *values, int *indexs, int n){
+	int mid, i, *valueL, *valueR, *indexL, *indexR;
+	if (n < 2) return;
+	mid = n/2;
+	
+	valueL = (int *)malloc(mid*sizeof(int));
+	valueR = (int *)malloc((n - mid)*sizeof(int));
+	
+	indexL = (int *)malloc(mid*sizeof(int));
+	indexR = (int *)malloc((n - mid)*sizeof(int));
+	for (i = 0; i < mid; i++){
+		valueL[i] = values[i];
+		indexL[i] = indexs[i];
+	}
+	
+	for (i = mid; i < n; i++){
+		valueR[i - mid] = values[i];
+		indexR[i - mid] = indexs[i];
+	}
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		mergeSortPara(valueL, indexL, mid);
+		#pragma omp section
+		mergeSortPara(valueR, indexR, n - mid);
+		
+	}
+	merge(values, valueL, valueR, indexs, indexL, indexR, mid, n - mid);
+	free(valueL);
+	free(valueR);
+	free(indexL);
+	free(indexR);
+	
+}
 
 
 
@@ -212,12 +215,15 @@ void copyMat(){
 
 
 int main(int argc, char **argv){
-	generateMat();
-	creatTempMat();
-	copyMat();
-	printMat(mat);
-	printf("\n");
-	way2Sort();
-	free(mat);
+	for (power = 4; power < 12; power++){
+		timer = 0;
+		N = pow(2, power);
+		generateMat();
+		creatTempMat();
+		copyMat();
+		way2Sort();
+		free(mat);
+		printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) timer);
+	}
 	exit(0);
 }
